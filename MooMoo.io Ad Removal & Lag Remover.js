886ed5e34a1f86d177e5773611c905c51acb2519
ISAGORIVAN <64742275+ISAGORIVAN@users.noreply.github.com>
2021-01-04 14:23:41 -0500

    Update MooMoo.io Ad Removal & Lag Remover.js

--- MooMoo.io Ad Removal & Lag Remover.js
+++ MooMoo.io Ad Removal & Lag Remover.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MooMoo.io Ad Removal and Lag Remover (Work In Progress)
-// @version      v1.6
+// @version      v1.8
 // @description  Removes ads in MooMoo.io. Fixes bugs that AdBlocker won't catch. This script is not backdoored and will never be. Please give feedback and tell us if this works. This also blurs the pixel a little to make your game run smoother.
 // @author       Morpehus and Ivan
 // @match        *://*.moomoo.io/*
@@ -13,12 +13,7 @@ a.remove();
 document.getElementsByClassName("menuCard")[4].remove();
 document.getElementsByClassName("menuCard")[2].remove();
 window.devicePixelRatio = 0.8;
-var clearIframes = setInterval(function(){
-    for (var i = 0; i < document.getElementsByTagName("iframe").length; i++){
-    document.getElementsByTagName("iframe")[i].remove();
-    }
-if(document.getElementsByTagName("iframe").length === 0){
-clearInterval(clearIframes);
-}
-}, 50);
 $("#moomooio_728x90_home").parent().css({display: "none"});
+ 
+/* Cookie Shit remover */
+document.getElementById("onetrust-consent-sdk").remove();
